3805bef353a1cc75fcd63f7df00db47a5c0e4aa8
danielhorton4001 <115335774+danielhorton4001@users.noreply.github.com>
2023-05-10 09:39:28 +1000

    Update image_popup.py
    Now takes main_controller arg

--- views/image_popup.py
+++ views/image_popup.py
@@ -12,5 +12,6 @@ class Popup(QWidget):
     If the slider is changed ore the left/right buttons are pressed
     the label should update to show (Image X/Y) and the image should update
     """
-    def __init__(self, parent=None) -> None:
+    def __init__(self, main_controller, parent=None) -> None:
         super().__init__(parent)
+        self._main_controller = main_controller
