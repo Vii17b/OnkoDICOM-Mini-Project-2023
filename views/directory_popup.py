706a0e6996025f3de4556c5348e4e4294ec961e9
danielhorton4001 <115335774+danielhorton4001@users.noreply.github.com>
2023-05-10 09:39:07 +1000

    Update directory_popup.py
    Now correct class and takes main_controller arg

--- views/directory_popup.py
+++ views/directory_popup.py
@@ -3,12 +3,14 @@ First time popup if the Config cannot find the hidden file
 Updates the Config accordingly
 """
 
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QDialog
 
 
-class Popup(QWidget):
+class Popup(QDialog):
     """
     Popup view that prompts the user to supply a default directory
     """
-    def __init__(self, parent=None) -> None:
+    def __init__(self, main_controller, parent=None) -> None:
         super().__init__(parent)
+
+        self._main_controller = main_controller
