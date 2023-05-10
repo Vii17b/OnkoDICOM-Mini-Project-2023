09baa705bedaa4bfe01b2f6d3d0e0f87dcfaaf03
PhilThom <106040732+PhilThom@users.noreply.github.com>
2023-05-10 05:24:52 +0000

    Update directory_popup.py
    redmine #2575

--- views/directory_popup.py
+++ views/directory_popup.py
@@ -1,16 +1,40 @@
-"""
-First time popup if the Config cannot find the hidden file
-Updates the Config accordingly
-"""
+"""First time popup if the Config cannot find the hidden file
+Updates the Config accordingly"""
 
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import (
+    QDialog, QFileDialog, QLabel, QPushButton, QHBoxLayout)
+from PySide6 import QDir
 
 
-class Popup(QDialog):
-    """
-    Popup view that prompts the user to supply a default directory
-    """
-    def __init__(self, main_controller, parent=None) -> None:
-        super().__init__(parent)
+class DirectoryView(QDialog):
+    """Popup view that prompts the user to supply a default directory"""
 
-        self._main_controller = main_controller
+    def __init__(self, controller):
+
+        """Creates the window for openening directory selection"""
+
+        super().__init__()
+        self.resize(400, 300)
+        self.controller = controller
+
+        self.setWindowTitle("Directory Selection")
+
+        # Create widgets
+        self.directory_info = QLabel()
+        self.directory_info.setText("Select the default directory for DICOM:")
+        self.select_directory = QPushButton("&Open")
+        self.select_directory.clicked.connect(self.selection)
+
+        # Create layout
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.directory_info)
+        self.layout.addWidget(self.select_directory)
+        self.setLayout(self.layout)
+
+    def selection(self):
+
+        """Opens the directory selection, returns the directory"""
+
+        directory = QFileDialog.getExistingDirectory(
+            self, "Select Directory", QDir.currentPath())
+        self.controller.check_config(directory)
