6bca0325525f4beea81cc7bf0d58ba2ecee19edf
danielhorton4001 <115335774+danielhorton4001@users.noreply.github.com>
2023-05-11 13:39:14 +1000

    Update image_popup.py

--- views/image_popup.py
+++ views/image_popup.py
@@ -22,7 +22,7 @@ class ImageView(QWidget):
         self.slider = QSlider(self, QtCore.Qt.Horizontal)
         self.slider.setOrientation(QtCore.Qt.Horizontal)
         self.slider.sliderMoved.connect(
-            lambda: controller.change_image_index(self.slider.value()))
+            lambda: controller.change_image_index(self.slider.value()-1))
         self.slider.setMinimumSize(QtCore.QSize(500, 10))
 
         # Create layout
