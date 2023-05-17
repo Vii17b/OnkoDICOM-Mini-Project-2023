799689d0bedb4f6655d86a0b843cbde5dcf3135b
danielhorton4001 <115335774+danielhorton4001@users.noreply.github.com>
2023-05-17 11:32:54 +1000

    Update main_model.py

--- models/main_model.py
+++ models/main_model.py
@@ -4,7 +4,7 @@ in this case, dicom directory and open image index
 Contains getters and setters for state information
 """
 
-from PySide6.QtWidgets import QObject
+from PySide6.QtCore import QObject
 
 
 class MainModel(QObject):
@@ -14,3 +14,5 @@ class MainModel(QObject):
     """
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
+        self.dicom_directory = None
+        self.image_index = 0
