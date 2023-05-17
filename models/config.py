b956d94ad8b5c6b2de1a8794d46ac4a37625d5f0
danielhorton4001 <115335774+danielhorton4001@users.noreply.github.com>
2023-05-17 11:32:17 +1000

    Update config.py

--- models/config.py
+++ models/config.py
@@ -11,7 +11,9 @@ class Config:
     Reads the config file
     """
 
-    def __init__(self):
+    def __init__(self, controller):
+        self.controller = controller
+
         # Locate the database file
         home_dir = os.path.expanduser("~")
         hidden_dir = home_dir + "/.OnkoDICOM"
@@ -24,14 +26,23 @@ class Config:
             if not os.path.exists(hidden_dir):
                 os.mkdir(hidden_dir)
 
-            # Writing random values currently, replace with actual data
-            self.write_config(dbfile, 0, home_dir, home_dir)
+            # Prompt the user for default directory, then save it to the config
+            self.controller.default_directory_prompt(self, dbfile)
+        else:
+            # If we don't have to wait for the user,
+            # respond to the controller immediately
+            self.send_results(dbfile)
 
+    def send_results(self, dbfile):
+        """
+        Tells the controller that the config file has been resolved
+        """
         # Read the config file and return
         results = self.read_config(dbfile)
         self.db_id = results[0]
         self.default_dir = results[1]
         self.csv_dir = results[2]
+        self.controller.config_resolved(self)
 
     def read_config(self, dbf):
         """
@@ -71,6 +82,14 @@ class Config:
 
         # Close the database connection
         conn.close()
+
+    def supply_default_directory(self, dbfile, home_dir):
+        """
+        Allows the directory window to write to the config
+        then lets the main controller continue
+        """
+        self.write_config(dbfile, 0, home_dir, home_dir)
+        self.send_results(dbfile)
 
 
 if __name__ == "__main__":
