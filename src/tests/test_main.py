17862375cc4512bb65ad3bb0b697c7ba4d7c9197
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-18 20:58:02 +1000

    Update test_main.py

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -3,17 +3,18 @@ Tests for main.py
 """
 
 import pytest
+from PySide6.QtWidgets import QApplication
+
 from main import App
 
 
 @pytest.fixture
-def app_test(qtbot):
+def app_test(QApplication):
     app_test = App()
     qtbot.addWidget(app_test)
     return app_test
 
 
 def test_startup(app_test):
-    # assert app_test.windowTitle() == "OnkoDICOM 2023 Mini Project"
-    # assert app_test.plot_w
-    pass
+    assert app_test.windowTitle() == "OnkoDICOM 2023 Mini Project"
+    assert app_test.plot_w
