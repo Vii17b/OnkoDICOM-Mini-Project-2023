21bb343066f9db7d3689a4964f789dddd94dbdc7
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-18 20:18:12 +1000

    Update test_main_view.py

--- src/tests/test_views/test_main_view.py
+++ src/tests/test_views/test_main_view.py
@@ -2,9 +2,12 @@
 Tests for main_view.py
 """
 
-from models.main_model import MainModel
-from controllers.main_controller import MainController
-from views.main_view import MainView
+# from models.main_model import MainModel
+from ...models.main_model import MainModel
+# from controllers.main_controller import MainController
+# from views.main_view import MainView
+from ...controllers.main_controller import MainController
+from ...views.main_view import MainView
 
 
 def test_main_view(qtbot):
