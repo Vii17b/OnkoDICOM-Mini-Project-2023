cf7e8682b5f6fcb4231cc4b8b74345f764aab0e6
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-18 20:13:09 +1000

    Update test_main_view.py

--- src/tests/test_views/test_main_view.py
+++ src/tests/test_views/test_main_view.py
@@ -2,11 +2,11 @@
 Tests for main_view.py
 """
 
-from models import main_model
-from controllers import main_controller
+from models.main_model import MainModel
+from controllers.main_controller import MainController
 from views.main_view import MainView
 
 
 def test_main_view(qtbot):
-    model_test = main_model.MainModel()
-    controller_test = main_controller.MainController(model)
+    model_test = MainModel()
+    controller_test = MainController(model)
