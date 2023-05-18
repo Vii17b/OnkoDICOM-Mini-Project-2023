c4c97402889316b4c5cd997d05482d717a3a53d0
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-18 18:45:42 +1000

    .

--- tests/test_controllers/test_main_controller.py
+++ tests/test_controllers/test_main_controller.py
@@ -2,12 +2,16 @@
 Tests for main_controller.py
 """
 
-from models import main_model
-from controllers import main_controller
+from models.dicom import DicomParser
+from models.config import Config
+
+from views.image_popup import ImageView
+from views.directory_popup import DirectoryView
 
 
 def test_main_controller(qtbot):
     model = main_model.MainModel()
 
     controller = main_controller.MainController(model)
+
     controller.change_selected_directory("dicom_file")
