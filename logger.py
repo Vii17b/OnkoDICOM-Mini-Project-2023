d41c83b103765b9672574961d79d92caf90b8898
danielhorton4001 <115335774+danielhorton4001@users.noreply.github.com>
2023-05-25 14:57:27 +1000

    Pycodestyle Corrections

---
+++ logger.py
@@ -0,0 +1,20 @@
+import logging
+
+
+class CustLogger:
+    def __init__(self, log_level=logging.DEBUG, name=__name__):
+
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(log_level)
+
+        file_holder = logging.FileHandler("src/log_file.log")
+
+        formatter = logging.Formatter('%(asctime)s '
+                                      '- %(levelname)s '
+                                      '- %(name)s '
+                                      '- : %(message)s',
+                                      datefmt='%m/%d/%Y %I:%M:%S %p')
+
+        file_holder.setFormatter(formatter)
+
+        self.logger.addHandler(file_holder)
