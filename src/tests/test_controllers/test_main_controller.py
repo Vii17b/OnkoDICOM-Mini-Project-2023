22d91a8d85800ada9f5e724c45f2f8110b4b9a45
danielhorton4001 <115335774+danielhorton4001@users.noreply.github.com>
2023-05-25 16:32:33 +1000

    Update test_main_controller.py

--- src/tests/test_controllers/test_main_controller.py
+++ src/tests/test_controllers/test_main_controller.py
@@ -11,3 +11,11 @@ def test_main_controller(qtbot):
     model_test = MainModel()
 
     controller_test = MainController(model_test)
+    controller_test._main_model
+    controller_test._config
+    controller_test._dicom_parser
+    controller_test.go_to_next_image()
+    controller_test.go_to_preivous_image()
+    
+    controller_test.open_image_viewer()
+    controller_test._image_popup.close()
